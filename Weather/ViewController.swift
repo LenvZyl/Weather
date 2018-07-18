f5b811b92e831d43f6ea76e074c79d123b854eec
Len X <lenvzyl@gmail.com>
2018-07-18 19:41:44 +0200

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Len X van Zyl on 7/18/18.
+//  Copyright © 2018 Len X van Zyl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
